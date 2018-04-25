fc65d928e5f83c8453ccea1174a62b0b8fe63e2e
Lam D. Nguyen <lam@popeye.snu.ac.kr>
2018-04-25 12:02:53 +0900

    commit

--- First_Blockchain_Python/blockchain.py
+++ First_Blockchain_Python/blockchain.py
@@ -9,4 +9,26 @@ class blockchain(object):
 
 		self.new_block(previous_hash=1, proof=100)
 
-	
+
+	def new_block(self, proof, previous_hash=None):
+		"""
+		proof: int , the proof given by the PoW algorithm
+		previous_hash: str , hash of previous block
+		return : dict, new block
+
+		"""
+		block = {
+		'index':len(self.chain)+1,
+		'timestamp':time(),
+		'transaction': self.current_transaction,
+		'proof': proof,
+		'previous_hash': previous_hash or self.hash(self.chain[-1]),
+		}
+
+		#reset current list of transactions
+		self.current_transaction[]
+		self.chain.append(block)
+		return block
+
+
+	def new_transaction(self, )
