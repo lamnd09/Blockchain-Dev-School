9365fc201bffa85247b4d58ebeb52d59ecaa8529
Lam D. Nguyen <lam@popeye.snu.ac.kr>
2018-05-09 19:04:46 +0900

    Blockchain

--- TiniestBlockchain/Blockchain.py
+++ TiniestBlockchain/Blockchain.py
@@ -1,5 +1,5 @@
 import hashlib as hasher
-import datetime as date
+from datetime import datetime
 
 class Block:
     def __init__(self, index, timestamp, data, previous_hash):
@@ -17,23 +17,22 @@ class Block:
         self.previous_hash = previous_hash
         self.hash = self.hash_block()
 
-        def hash_block(self):
-            sha = hasher.sha256()
-            sha.update(str(self.index) +
-                       str(self.timestamp)+
-                       str(self.data) +
-                       str(self.previous_hash))
-            return sha.hexdigest()
+    def hash_block(self):
+        sha = hasher.sha256()
+        sha.update(str(self.index).encode('utf-8') + str(self.timestamp).encode('utf-8') + str(self.data).encode(
+            'utf-8') + str(self.previous_hash).encode('utf-8'))
+
+        return sha.hexdigest()
 
 
 # Genesis block
 def create_genesis_block():
-    return Block(0, date.datetime.now(), "genesis", "0")
+    return Block(0, datetime.now(), "genesis", "0")
 
 
 def next_block(last_block):
     this_index = last_block.index +1
-    this_timestamp = date.datetime.now()
+    this_timestamp = datetime.now()
     this_data = "data" + str(this_index)
     this_hash = last_block.hash
     return Block(this_index, this_timestamp, this_data,this_hash)
@@ -50,5 +49,5 @@ for i in range(0, number_of_blocks):
     blockchain.append(block_to_add)
     previous_block = block_to_add
 
-    print(" Block #{} has been added").format(block_to_add.index)
-    print(" Hash: #{}\n").format(block_to_add.hash)
+    print(" Block #{} has been added".format(block_to_add.index))
+    print(" Hash: #{}\n".format(block_to_add.hash))
