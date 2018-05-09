798718ee48bb4f2f01e686ccf80903c0b49a5c5b
Lam D. Nguyen <lam@popeye.snu.ac.kr>
2018-05-09 18:51:15 +0900

    Creating a Blockchain

--- TiniestBlockchain/Blockchain.py
+++ TiniestBlockchain/Blockchain.py
@@ -31,4 +31,24 @@ def create_genesis_block():
     return Block(0, date.datetime.now(), "genesis", "0")
 
 
+def next_block(last_block):
+    this_index = last_block.index +1
+    this_timestamp = date.datetime.now()
+    this_data = "data" + str(this_index)
+    this_hash = last_block.hash
+    return Block(this_index, this_timestamp, this_data,this_hash)
 
+
+# Create a blockchain
+blockchain = [create_genesis_block()]
+previous_block = blockchain[0]
+
+number_of_blocks = 30
+
+for i in range(0, number_of_blocks):
+    block_to_add = next_block(previous_block)
+    blockchain.append(block_to_add)
+    previous_block = block_to_add
+
+    print(" Block #{} has been added").format(block_to_add.index)
+    print(" Hash: #{}\n").format(block_to_add.hash)
